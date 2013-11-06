1b956cc5bcddffec52558c77b4e8857c7d8119ef
chy800 <chyzhj2@163.com>
2013-11-06 15:24:55 +0800

    Update TestMain.java

--- Test2/src/TestMain.java
+++ Test2/src/TestMain.java
@@ -1,9 +1,7 @@
-
 public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sfsdfdfsd");
 		System.out.println("sfsdfdfsd");
 		System.out.println("sfsdfdfsd");
 	}
